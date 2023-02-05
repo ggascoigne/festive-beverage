553722b902e30b70ae71c0a7b54bbf549e8663ae
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2023-02-05 11:50:51 -0800

    Remove wireit

--- next.config.js
+++ next.config.js
@@ -4,11 +4,17 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 })
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: true,
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+  })
+)
---
+++ todo.md
@@ -0,0 +1,9 @@
+# Todo
+
+ensure that the sqlite database gets shared to the ui somehow - as a url ideally
+
+add the sqlite-worker (see the example in /Users/ggp/dev/hs-git/northstar/table-example), loading the database
+
+test querying it
+
+then migrate the UI to use it.
