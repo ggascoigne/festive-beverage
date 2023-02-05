31ea205b5588d0dca209c6b959a444516bbf710e
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2023-02-05 13:43:51 -0800

    pwa test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+const defaultRuntimeCaching = require('next-pwa/cache')
 
 const withPWA = require('next-pwa')({
   dest: 'public',
+  runtimeCaching: [
+    {
+      urlPattern: /\/api\/.+$/,
+      method: 'GET',
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'api-data',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+    ...defaultRuntimeCaching,
+  ],
 })
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
