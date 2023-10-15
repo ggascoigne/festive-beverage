663f12e754854abdb5cecf2f78278cf84bf903b7
Guy Gascoigne-Piggford <guy@wyrdrune.com>
2023-10-15 12:40:20 -0700

    minor updates, useGraphql cleanup

--- src/client/useGraphQL.ts
+++ src/client/useGraphQL.ts
@@ -3,28 +3,44 @@ import { UseQueryOptions, useQuery } from '@tanstack/react-query'
 import { Kind, ASTNode, OperationDefinitionNode } from 'graphql'
 import request from 'graphql-request'
 
+import { QueryError } from './error'
+
 const isOperationDefinition = (def: ASTNode): def is OperationDefinitionNode => def.kind === Kind.OPERATION_DEFINITION
+
+type TQueryKey<TVariables> = [string, TVariables | undefined]
+
+type TOptions<TResult, TVariables> = Omit<
+  UseQueryOptions<TResult, QueryError, TResult, TQueryKey<TVariables>>,
+  'queryKey' | 'initialData'
+> & {
+  initialData?: TResult | (() => TResult)
+}
+
+type WithOptions<TResult, TVariables> = {
+  variables?: TVariables
+  options?: TOptions<TResult, TVariables>
+}
+
+const isWithOptions = <TResult, TVariables>(
+  value: TVariables | WithOptions<TResult, TVariables>
+): value is WithOptions<TResult, TVariables> => value && typeof value === 'object' && Object.hasOwn(value, 'options')
+
+const parseVars = <TResult, TVariables>(params?: TVariables | WithOptions<TResult, TVariables>) => {
+  if (params && isWithOptions(params)) {
+    return [params.variables, params.options] as const
+  } else {
+    return [params, undefined] as const
+  }
+}
 
 export function useGraphQL<TResult, TVariables>(
   document: TypedDocumentNode<TResult, TVariables>,
-  variables?:
-    | TVariables
-    | {
-        variables?: TVariables
-        options?: Omit<UseQueryOptions<TResult, unknown, TResult, TVariables[]>, 'queryKey' | 'initialData'> & {
-          initialData?: TResult | (() => TResult)
-        }
-      }
+  variables?: TVariables | WithOptions<TResult, TVariables>
 ) {
-  const operationName = document.definitions.find(isOperationDefinition)?.name?.value
-  let vars = variables
-  let opts
-  if (variables && Object.hasOwn(variables, 'operations')) {
-    vars = (variables as any).variables
-    opts = (variables as any).options
-  }
-  return useQuery(
-    [operationName, vars] as const,
+  const operationName = document.definitions.find(isOperationDefinition)?.name?.value ?? 'missing operation'
+  const [vars, opts] = parseVars(variables)
+  return useQuery<TResult, QueryError, TResult, TQueryKey<TVariables>>(
+    [operationName, vars],
     async ({ queryKey }) => request(`/api/graphql/${operationName}`, document, queryKey[1] ? queryKey[1] : undefined),
     opts
   )
